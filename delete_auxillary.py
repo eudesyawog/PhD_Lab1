927736f2cfd9e5a2a9169858be010c5ac80d782e
eudesyawog <gyjeaneudes@gmail.com>
2018-11-17 23:21:14 +0100

    Delete aux.xml files

---
+++ delete_auxillary.py
@@ -0,0 +1,35 @@
+import glob
+import os
+from pprint import pprint
+
+class AuxXML :
+    """
+    List aux.xml files in inputs folder and delete them
+    """
+
+    def __init__(self, lstFolders=""):
+        object.__init__(self)
+        self.__lstFolders = lstFolders
+    
+    def __str__(self) : 
+        return "Actual Folders : " + self.__lstFolders
+    
+    def __repr__(self):
+        str(self.__dict__)
+
+    def add_folder(self, inFolder):
+        self.__lstFolders.append(inFolder)
+        pprint (self.__lstFolders)
+    
+    def clean(self):
+        lstAux = []
+        for Folder in self.__lstFolders :
+            lstAux.extend(glob.glob(Folder+os.sep+"**"+os.sep+"*aux.xml",recursive=True))
+        print (str(len(lstAux)) + " Auxillary Files found will be deleted")
+        for File in lstAux :
+            os.remove(File)
+
+if __name__=="__main__" : 
+    lstFolders = ["/media/je/JE3/lab1/"]
+    AuxXMLObject = AuxXML(lstFolders)
+    AuxXMLObject.clean()
