12f7abc3b0150b5394697ebe2e1977550409a57a
eudesyawog <gyjeaneudes@gmail.com>
2018-11-30 14:46:47 +0100

    major changes

--- preprocess.py
+++ preprocess.py
@@ -13,6 +13,7 @@ import subprocess
 import fiona
 import rasterio
 import gdal
+import osr
 import numpy as np
 import pandas as pd
 from affine import Affine
@@ -52,58 +53,87 @@ class ExtractZip :
         end_time = time.time()
         print("Time for unzipping : %ssecs" % (end_time - start_time))         
     
-class Mosaic_Clip :
+class Mos_Clip :
     """
     Browse unzip folders, Mosaic bands of interest if necessary, Clip them as same as 
     corresponding clouds mask using a vector file extent and Organize images by date
     """
-    def __init__(self, unzipfolder, vectorfile, lstBand=["B2","B3","B4","B8","CLM"], product="FRE", NUM_WORKERS=10):
+    def __init__(self, unzipfolder, vectorfile, epsg, lstBand=["B2","B3","B4","B8","CLM"], product="FRE", NUM_WORKERS=10):
         self._unzipfolder = unzipfolder
         self._vectorfile = vectorfile
         self._lstBand = lstBand
         self._product = product
         self._NUM_WORKERS = NUM_WORKERS
+        self._epsg = epsg
     
     def _clip(self,inFile,outFile,band): 
+        
         if self._recover_metadata :
             vec = fiona.open(self._vectorfile)
             self._ulx = vec.bounds[0]
             self._uly = vec.bounds[3]
-            lrx = vec.bounds[2]
-            lry = vec.bounds[1]
+            self._lrx = vec.bounds[2]
+            self._lry = vec.bounds[1]
             vec = None
 
-            with rasterio.open(inFile) as ds:
-                originX = ds.affine[2]
-                originY = ds.affine[5]
-                self._pixelWidth = ds.affine[0]
-                self._pixelHeight = ds.affine[4]
+            ds = gdal.Open(inFile, gdal.GA_ReadOnly)
+            GeoT = ds.GetGeoTransform()
+            self._xsize = int((self._lrx - self._ulx)/GeoT[1])
+            self._ysize = int((self._uly - self._lry)/GeoT[1])
+            self._GeoT = (self._ulx,GeoT[1],GeoT[2],self._uly,GeoT[4],GeoT[5]) 
+            ds = None
 
-                self._xOffset = int((self._ulx- originX) / self._pixelWidth)
-                self._yOffset = int((self._uly - originY) / self._pixelHeight)
-                self._xsize = int((lrx - self._ulx)/self._pixelWidth)
-                self._ysize = int((self._uly - lry)/self._pixelWidth)
+            self._proj = osr.SpatialReference()
+            self._proj.ImportFromEPSG(self._epsg)
         
         if band == "CLM" :
-            with rasterio.open(inFile) as ds:
-                mask_array = ds.read(1,window=((self._yOffset,self._yOffset+self._ysize),(self._xOffset, self._xOffset+self._xsize)))
-                mask_array = np.where(mask_array>0,1,0)
+            mem_drv = gdal.GetDriverByName('MEM')
+            dest = mem_drv.Create('', self._xsize, self._ysize, 1, gdal.GDT_Byte) # dtype 1 for uint8 or gdal.GDT_Byte
+            dest.SetGeoTransform(self._GeoT)
+            dest.SetProjection(self._proj.ExportToWkt())
 
-                profile = ds.profile
-                profile.update(width=self._xsize,height=self._ysize,transform=(self._ulx,self._pixelWidth,0.,self._uly,0.,self._pixelHeight))
-                
-            with rasterio.open(outFile,'w', **profile) as outds :
-                outds.write(mask_array.astype(rasterio.uint8),1)
-        else : 
-            with rasterio.open(inFile) as ds:
-                band_array = ds.read(1,window=((self._yOffset,self._yOffset+self._ysize),(self._xOffset, self._xOffset+self._xsize)))
-                band_array = np.where(band_array!=-10000,band_array+1,-10000) # +1 for flat reflectance
+            gdal.Warp(dest, inFile, dstSRS="EPSG:%s"%self._epsg, outputBounds=(self._ulx, self._lry, self._lrx, self._uly))
+            mask_array = dest.GetRasterBand(1).ReadAsArray()
+            mask_array = np.where(mask_array>0,1,0) # Strict Cloud Mask
 
-                profile = ds.profile
-                profile.update(width=self._xsize,height=self._ysize,nodata=-10000,transform=(self._ulx,self._pixelWidth,0.,self._uly,0.,self._pixelHeight))
+            driver=gdal.GetDriverByName('GTiff')
+            ds = driver.Create(outFile, self._xsize, self._ysize, 1, gdal.GDT_Byte)# dtype 1 for uint8 or gdal.GDT_Byte
+            ds.SetGeoTransform(self._GeoT)
+            ds.SetProjection(self._proj.ExportToWkt())
+            ds.GetRasterBand(1).WriteArray(mask_array)
             
-            with rasterio.open(outFile,'w', **profile) as outds :
-                    outds.write(band_array.astype(rasterio.int16),1)
+            mem_drv = None
+            driver = None
+            dest = None
+            ds = None
+            
+        else :
+            mem_drv = gdal.GetDriverByName('MEM')
+            dest = mem_drv.Create('', self._xsize, self._ysize, 1, gdal.GDT_Int16) # dtype 3 for int16 or gdal.GDT_Int16
+            dest.SetGeoTransform(self._GeoT)
+            dest.SetProjection(self._proj.ExportToWkt())
+            # Prefilled solution
+            dest.GetRasterBand(1).SetNoDataValue(-10000)
+            fill_array = np.ndarray(shape=(self._ysize, self._xsize))
+            fill_array.fill(-10000)
+            dest.GetRasterBand(1).WriteArray(fill_array)
+
+            # cutlineDSName=self._vectorfile, cropToCutline=True,
+            gdal.Warp(dest, inFile, dstSRS="EPSG:%s"%self._epsg, outputBounds=(self._ulx, self._lry, self._lrx, self._uly), srcNodata = -10000, dstNodata = -10000)
+            band_array = dest.GetRasterBand(1).ReadAsArray()
+            band_array = np.where(band_array!=-10000,band_array+1,-10000) # +1 for flat reflectance
+
+            driver=gdal.GetDriverByName('GTiff')
+            ds = driver.Create(outFile, self._xsize, self._ysize, 1, gdal.GDT_Int16)# dtype 3 for int16 or gdal.GDT_Int16
+            ds.SetGeoTransform(self._GeoT)
+            ds.SetProjection(self._proj.ExportToWkt())
+            ds.GetRasterBand(1).WriteArray(band_array)
+            ds.GetRasterBand(1).SetNoDataValue(-10000)
+            
+            mem_drv = None
+            driver = None
+            dest = None
+            ds = None
         
     def process(self, date):
         outPath = os.path.dirname(self._unzipfolder) + os.sep + "MOS_CLIP" + os.sep + date
@@ -117,13 +147,13 @@ class Mosaic_Clip :
             for band in self._lstBand :
                 # Mosaic 
                 if band == "CLM":
-                    Command = ["gdalwarp",'-overwrite', "-multi","-ot","Byte"]
+                    Command = ["gdalwarp","-t_srs", "EPSG:%s"%self._epsg,'-overwrite', "-multi","-ot","Byte"]
                     for d in range (len(lstFolders)):
                         for File in os.listdir(os.path.join(lstFolders[d],"MASKS")):
                             if re.match("(.*)_{}".format("CLM_R1.tif"),File) and File.endswith(".tif") :
                                 Command += [os.path.join(lstFolders[d],"MASKS",File)]
                 else :
-                    Command = ["gdalwarp",'-overwrite', "-srcnodata", "-10000" , "-dstnodata", "-10000", "-multi","-ot","Int16"]
+                    Command = ["gdalwarp","-t_srs", "EPSG:%s"%self._epsg,'-overwrite', "-srcnodata", "-10000" , "-dstnodata", "-10000", "-multi","-ot","Int16"]
                     for d in range (len(lstFolders)):
                         for File in os.listdir(lstFolders[d]):
                             if re.match("(.*)_{}_{}(.*)".format(self._product,band),File) and File.endswith(".tif") and not File.endswith("B8A.tif"):
@@ -179,7 +209,7 @@ class Mosaic_Clip :
         end_time = time.time()
         print("Time for process : %ssecs" % (end_time - start_time))
 
-class Cloud_Status :
+class Cloud_NoData_Status :
     """
     Browse Mosaic and Clipping Images Folders and Compute Cloud Percent from Cloud Masks
     """
@@ -192,18 +222,27 @@ class Cloud_Status :
         lstFolders = [folder for folder in os.listdir(self._inPath) if os.path.isdir(os.path.join(self._inPath,folder))]
         lstFolders.sort()
 
-        outCSV = os.path.join(self._inPath,"cloud_status.csv")
+        outCSV = os.path.join(self._inPath,"cloud_nodata_status.csv")
         dic = {}
 
         for d in range(len(lstFolders)):
+            dic.setdefault('Date',[]).append(lstFolders[d])
             for File in os.listdir(os.path.join(self._inPath,lstFolders[d])):
+
+                if re.match("{}_{}_(.*)".format("B2",lstFolders[d]),File) and File.endswith(".tif"):
+                    with rasterio.open(os.path.join(self._inPath,lstFolders[d],File)) as ds :
+                        b2_band = ds.read(1)
+                        
                 if re.match("{}_{}_(.*)".format("CLM",lstFolders[d]),File) and File.endswith(".tif"):
                     with rasterio.open(os.path.join(self._inPath,lstFolders[d],File)) as ds :
                         cld_band = ds.read(1)
-                        cld_percent = round((np.count_nonzero(cld_band==1) * 100) / (ds.width * ds.height))
-                    dic.setdefault('Date',[]).append(lstFolders[d])
-                    dic.setdefault('Cloud_Percentage',[]).append(cld_percent)
-        
+
+            nodata_pixels_nb = np.count_nonzero(b2_band==-10000)
+            nodata_percent = round(((nodata_pixels_nb * 100) / (ds.width * ds.height)),2)
+            cld_percent = round(((np.count_nonzero(cld_band==1) * 100) / ((ds.width * ds.height)-nodata_pixels_nb )),2)
+            dic.setdefault('Cloud_Percentage',[]).append(cld_percent)
+            dic.setdefault('NoData_Percentage',[]).append(nodata_percent)
+                
         df = pd.DataFrame.from_dict(dic)
         df.to_csv(outCSV,index=False)
 
@@ -212,14 +251,37 @@ class Concatenate :
     Concatenate images of the times series per band
     """
 
-    def __init__(self, inPath, bands=["B2","B3","B4","B8","CLM"]):
+    def __init__(self, inPath, csvFile, cld = 50, ndt = 15, bands=["B2","B3","B4","B8","GAPF_MASK"]):
         self._inPath = inPath
         self._bands = bands
+        self._csvFile = csvFile
+        self._cld = cld
+        self._ndt = ndt
     
     def do(self):
-        
-        dates = [folder for folder in os.listdir(self._inPath) if os.path.isdir(os.path.join(self._inPath,folder))]
-        dates.sort()
+
+        df = pd.read_csv(self._csvFile)
+        dates = df["Date"][(df['Cloud_Percentage'] <= self._cld) & (df['NoData_Percentage'] <= self._ndt)].astype('str').tolist()
+
+        for m in range(len(dates)):
+            for File in os.listdir(os.path.join(self._inPath,dates[m])):
+                if re.match("{}_{}(.*)".format("B2",dates[m]),File):
+                    with rasterio.open(os.path.join(self._inPath,dates[m],File)) as ds :
+                        band_array = ds.read(1)
+                    break
+            for File in os.listdir(os.path.join(self._inPath,dates[m])):
+                if re.match("{}_{}(.*)".format("CLM",dates[m]),File):
+                    with rasterio.open(os.path.join(self._inPath,dates[m],File)) as ds :
+                        mask_array = ds.read(1)
+                        profile = ds.profile
+                    break
+            
+            gapf_mask = np.copy(mask_array)
+            gapf_mask = np.where(band_array==-10000,1,mask_array)
+            gmFile = "GAPF_MASK_"+File.split('_',1)[1]
+
+            with rasterio.open(os.path.join(self._inPath,dates[m],gmFile), 'w', **profile) as ds :
+                ds.write(gapf_mask,1)
 
         outPath = os.path.dirname(self._inPath)+os.sep+"GAPF"
         if not os.path.isdir(outPath):
@@ -238,20 +300,13 @@ class Concatenate :
                 for File in os.listdir(os.path.join(self._inPath,dates[m])):
                     if re.match("{}_{}(.*)".format(band,dates[m]),File):
                         Command += [os.path.join(self._inPath,dates[m],File)]
-            if band != "CLM":
+            if band != "CLM" or band != "GAPF_MASK":
                 Command += ['-out',os.path.join(outPath,outName.format(band)),'int16']
-            else:
+            else :
                 Command += ['-out',os.path.join(outPath,outName.format(band)),'uint8']
             Command += ['-ram','32768']
 
             subprocess.call(Command,shell=False)
-            
-            if band != "CLM" :
-                ds = gdal.Open(os.path.join(outPath,outName.format(band)), gdal.GA_Update)
-                for i in range(ds.RasterCount):
-                    ds.GetRasterBand(i+1).SetNoDataValue(-10000)
-                    band = None
-                ds = None
 
 class GapFilling :
     """
@@ -271,11 +326,11 @@ class GapFilling :
         
         inName = os.path.join(self._inPath,"{}_"+ptrn)
         outName = os.path.join(self._inPath,"{}_"+ptrn.strip('.tif')+"_GAPF.tif")
-        cloudfile = os.path.join(self._inPath,"CLM_"+ptrn)
+        maskfile = os.path.join(self._inPath,"GAPF_MASK_"+ptrn)
         datesfile  = os.path.join(self._inPath,"dates.txt")
 
         for band in self._bands:
-            Command = ['otbcli_ImageTimeSeriesGapFilling', '-in', inName.format(band),'-mask', cloudfile, 
+            Command = ['otbcli_ImageTimeSeriesGapFilling', '-in', inName.format(band),'-mask', maskfile, 
                        '-out', outName.format(band), 'int16', '-comp', '1', '-it', self._method,'-id', datesfile, '-ram', '32768']
             subprocess.call(Command,shell=False)
         
@@ -295,16 +350,17 @@ if __name__ == '__main__':
 
     # Mosaicking & Clipping
     unzipfolder = "/media/je/SATA_1/Lab1/REUNION/OUTPUT/UNZIP"
-    vectorfile = "/media/je/SATA_1/Lab1/REUNION/BD_GABIR_2017_v3/BD_GABIR_2017_v3.shp"
-    mos_clipObject = Mosaic_Clip(unzipfolder,vectorfile)
+    vectorfile = "/media/je/SATA_1/Lab1/REUNION/EXTENT/REUNION_BUF1000.shp"
+    mos_clipObject = Mos_Clip(unzipfolder,vectorfile,epsg=32740)
     mos_clipObject.parallelize()
 
     # Cloud status
     inPath = "/media/je/SATA_1/Lab1/REUNION/OUTPUT/MOS_CLIP"
-    cld_Object = Cloud_Status(inPath)
-
+    Cloud_NoData_Status(inPath)
+    
     # Concatenate Images
-    concatenateObject = Concatenate(inPath)
+    csvFile = "/media/je/SATA_1/Lab1/REUNION/OUTPUT/MOS_CLIP/cloud_nodata_status.csv"
+    concatenateObject = Concatenate(inPath, csvFile)
     concatenateObject.do()
 
     # GapFilling 
@@ -318,25 +374,27 @@ if __name__ == '__main__':
 
     # Unzipping
     zipfolder = "/media/je/SATA_1/Lab1/DORDOGNE/IMAGES"
+    zipfolder = "/media/je/SATA_1/Lab1/DORDOGNE/NEW_IMAGES"
     zipobject = ExtractZip(zipfolder)
     zipobject.extract_files()
 
     # Mosaic and Clipping
     unzipfolder = "/media/je/SATA_1/Lab1/DORDOGNE/OUTPUT/UNZIP"
     vectorfile = "/media/je/SATA_1/Lab1/DORDOGNE/SOURCE_VECTOR/ZONE_ENV.shp"
-    mos_clipObject = Mosaic_Clip(unzipfolder,vectorfile)
+    mos_clipObject = Mos_Clip(unzipfolder,vectorfile,epsg=2154)
     mos_clipObject.parallelize()
 
     # Cloud Status
     inPath = "/media/je/SATA_1/Lab1/DORDOGNE/OUTPUT/MOS_CLIP"
-    cld_Object = Cloud_Status(inPath)
+    Cloud_NoData_Status(inPath)
 
     # Concatenate Images
-    concatenateObject = Concatenate(inPath)
+    csvFile = "/media/je/SATA_1/Lab1/DORDOGNE/OUTPUT/MOS_CLIP/cloud_nodata_status.csv"
+    concatenateObject = Concatenate(inPath,csvFile)
     concatenateObject.do()
 
     # GapFilling
-    inPath = "/media/je/SATA_1/Lab1/REUNION/OUTPUT/GAPF"
+    inPath = "/media/je/SATA_1/Lab1/DORDOGNE/OUTPUT/GAPF"
     GapFillingObject = GapFilling(inPath)
     GapFillingObject.do()
 
