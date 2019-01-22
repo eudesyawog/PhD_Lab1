3bd23eeaa4a172588b60c4f54d1d6e29a5f5ae65
eudesyawog <gyjeaneudes@gmail.com>
2019-01-22 18:09:42 +0100

    init process_end file

---
+++ process_end.py
@@ -0,0 +1,45 @@
+import glob
+import os
+import re
+import rasterio
+import gdal
+import ogr 
+from pprint import pprint
+import time
+from datetime import datetime
+import random
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import subprocess
+from multiprocessing import Pool
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, cohen_kappa_score, f1_score
+import matplotlib.pyplot as plt
+
+class Geodesic_Morpho_Operation :
+    """
+    Performs geoedesic morphological operations on a grayscale input image
+    """
+
+    def __init__(self):
+        """
+        """
+
+class Classifier :
+    def __init__(self,inPath,ground_truth,niter=5,DateTime=None):
+        """
+        """
+
+if __name__ == '__main__':
+
+    # ========
+    # REUNION
+    # ========
+
+    # ========
+    # DORDOGNE
+    # ========
