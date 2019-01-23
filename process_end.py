f01a82e02461e6fedf581829f86d70e05413962c
eudesyawog <gyjeaneudes@gmail.com>
2019-01-23 10:10:55 +0100

    tuesday night commit

--- process_end.py
+++ process_end.py
@@ -19,15 +19,36 @@ from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, cohen_kappa_score, f1_score
 import matplotlib.pyplot as plt
+from skimage.morphology import erosion, dilation, opening, closing, disk,  
 
 class Geodesic_Morpho_Operation :
     """
     Performs geoedesic morphological operations on a grayscale input image
     """
 
-    def __init__(self):
+    def __init__(self, inPath, NUM_WORKERS=5):
         """
         """
+        self._inPath = inPath
+        self._outPath = os.path.dirname(inPath)+os.sep+"EMP_Geodesic_%s"%os.path.basename(inPath).split('_')[-1]
+        if not os.path.isdir(self._outPath):
+            os.makedirs(self._outPath)
+        self._NUM_WORKERS = NUM_WORKERS
+    
+    def _process_mp (self,File):
+        
+        tmp_folder = self._outPath+os.sep+os.path.basename(File).split('_',1)[0]+"_EMP"
+        tmp_file = os.path.basename(File).replace(".tif","_C%s_%s_%s.tif")
+        if not os.path.isdir(tmp_folder):
+            os.makedirs(tmp_folder)
+        
+        with rasterio.open(File) as ds:
+            count = ds.count       
+            for i in range(1,count+1):
+                for j in range(3,8,2):
+                for k in ("dilate","erode","opening","closing"):
+                    selem = disk(j)
+                    eroded = erosion(ds.read(i), selem)
 
 class Classifier :
     def __init__(self,inPath,ground_truth,niter=5,DateTime=None):
